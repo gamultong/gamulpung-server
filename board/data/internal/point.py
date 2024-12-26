0c4571b6b18044aa0119c283aa09a4cd7ea6ec4c
onee-only <kimww0306@gmail.com>
2024-12-26 00:33:10 +0000

    point에 bytes marsha/unmarshal 추가

--- board/data/internal/point.py
+++ board/data/internal/point.py
@@ -8,3 +8,14 @@ class Point:
 
     def copy(self):
         return Point(self.x, self.y)
+
+    def marshal_bytes(self) -> bytes:
+        x_b = self.x.to_bytes(length=8, signed=True)
+        y_b = self.y.to_bytes(length=8, signed=True)
+        return x_b + y_b
+
+    @staticmethod
+    def unmarshal_bytes(b: bytes):
+        x = int.from_bytes(bytes=b[:8], signed=True)
+        y = int.from_bytes(bytes=b[8:], signed=True)
+        return Point(x, y)
