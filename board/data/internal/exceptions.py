4c58a5c5673031b53bd3b1b10bd12943bec60587
onee-only <kimww0306@gmail.com>
2024-12-28 04:07:07 +0000

    board exception msg 대신 __str__ 사용하도록 변경

--- board/data/internal/exceptions.py
+++ board/data/internal/exceptions.py
@@ -1,7 +1,15 @@
 class InvalidTileException(Exception):
-    msg: str = "invalid tile"
+    def __init__(self, tile: dict):
+        self.tile = tile
+
+    def __str__(self):
+        return f"invalid tile: {self.tile}"
 
 
 class InvalidDataLengthException(Exception):
     def __init__(self, expected: int, actual: int):
-        self.msg = f"invalid data length. expected: {expected}, actual: {actual}"
+        self.expected = expected
+        self.actual = actual
+
+    def __str__(self):
+        return f"invalid data length. expected: {self.expected}, actual: {self.actual}"
