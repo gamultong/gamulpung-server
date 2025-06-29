bc79bed929f59f396b700bda371eb7f0185dd81d
byundojin <byundojin0216@gmail.com>
2025-06-29 05:27:58 +0000

    util pylance 맞추기

--- receiver/internal/utils.py
+++ receiver/internal/utils.py
@@ -1,7 +1,7 @@
 from data.conn.event import ServerEvent
 
 
-async def multicast(target_conns: list[str], event: ServerEvent):
+async def multicast(target_conns: list[str], event: ServerEvent.Base):
     # TODO: event broker가 아닌 connection에 직접 쏴 줄 수 있도록 바꾸기
     pass
     # if len(target_conns) == 0:
@@ -19,7 +19,7 @@ async def multicast(target_conns: list[str], event: ServerEvent):
     # )
 
 
-async def broadcast(event: ServerEvent):
+async def broadcast(event: ServerEvent.Base):
     # TODO: 위와 같음.
     pass
     # await EventBroker.publish(
