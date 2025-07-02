b80ab899c3f1c237684d3ab278f88eadcce81df5
onee-only <kimww0306@gmail.com>
2025-07-02 02:57:30 +0000

    cursor death receiver 추가

--- receiver/internal/cursor.py
+++ receiver/internal/cursor.py
@@ -3,6 +3,7 @@ from event.message import Message
 
 from data.payload import DataPayload
 from data.cursor import Cursor
+from data.score import Score
 from data.conn.event import ServerEvent
 
 from handler.cursor import CursorEvent
@@ -22,8 +23,29 @@ class CursorReceiver():
 
         await multicast_my_cursor(cursor)
 
+    @EventBroker.add_receiver(CursorEvent.DEATH)
+    @staticmethod
+    async def cursor_death(message: Message[DataPayload[Cursor]]):
+        cursor = message.payload.data
+        assert cursor is not None
+
+        score = await ScoreHandler.get(cursor.id)
+        await move_to_persistent_scores(score)
+
+        await reserve_revival(cursor)
+
 
 async def multicast_my_cursor(cursor: Cursor):
     event = ServerEvent.MyCursor(id=cursor.id)
 
     await multicast(target_conns=[cursor.conn_id], event=event)
+
+
+async def move_to_persistent_scores(score: Score):
+    # TODO: Persistent score storage.
+    await ScoreHandler.delete(id=score.id)
+
+
+async def reserve_revival(cursor: Cursor):
+    # TODO: Create scheduler.
+    pass
--- receiver/test/cursor_test.py
+++ receiver/test/cursor_test.py
@@ -3,12 +3,15 @@ from data.cursor import Cursor, Color
 from data.conn.event import ServerEvent
 from data.payload import DataPayload
 from data.score import Score
+from datetime import datetime, timedelta
 
 from event.message import Message
 
 from receiver import CursorReceiver
 
 from handler.cursor import CursorEvent
+from handler.score import ScoreHandler
+
 from unittest import TestCase, IsolatedAsyncioTestCase as AsyncTestCase
 from tests.utils import PathPatch
 from unittest.mock import AsyncMock, MagicMock, call
@@ -22,7 +25,7 @@ patch = PathPatch(MOCK_PATH)
 class CursorReceiver_TestCase(AsyncTestCase):
     @patch("multicast_my_cursor")
     @patch("ScoreHandler.create")
-    async def test_normal(self, create: AsyncMock, multicast_my_cursor: AsyncMock):
+    async def test_cursor_created_normal(self, create: AsyncMock, multicast_my_cursor: AsyncMock):
         (cursor,) = get_cur_set(1)
 
         message = Message(
@@ -34,3 +37,24 @@ class CursorReceiver_TestCase(AsyncTestCase):
 
         create.assert_called_once_with(id=cursor.id)
         multicast_my_cursor.assert_called_once_with(cursor)
+
+    @patch("ScoreHandler.get")
+    @patch("move_to_persistent_scores")
+    @patch("reserve_revival")
+    async def test_cursor_death_normal(self, reserve: AsyncMock, persist: AsyncMock, get: AsyncMock):
+        (cursor,) = get_cur_set(1)
+
+        cursor.revive_at = datetime.now() + timedelta(hours=1)
+        score = Score(cursor_id=cursor.id, value=10, rank=None)
+
+        get.return_value = score
+
+        message = Message(
+            event=CursorEvent.DEATH,
+            payload=DataPayload(id=cursor.id, data=cursor)
+        )
+
+        await CursorReceiver.cursor_death(message)
+
+        reserve.assert_called_once_with(cursor)
+        persist.assert_called_once_with(score)
