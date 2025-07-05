9705d5d0f736812af31fdf6cecb8d9975e005dce
onee-only <kimww0306@gmail.com>
2025-07-05 15:12:51 +0000

    notify window changed receiver 추상화 수준 변경 및 테스트 재구현

--- receiver/internal/cursor/notify_window_changed.py
+++ receiver/internal/cursor/notify_window_changed.py
@@ -3,7 +3,7 @@ from event.message import Message
 
 from data.payload import DataPayload
 from data.cursor import Cursor
-from data.board import Tiles
+from data.board import Tiles, PointRange
 from data.conn.event import ServerEvent
 
 from handler.cursor import CursorEvent, CursorHandler, CursorException
@@ -11,16 +11,6 @@ from handler.board import BoardHandler
 from handler.score import ScoreHandler
 
 from ..utils import multicast
-
-
-def validate(old: Cursor, new: Cursor):
-    if old.position != new.position:
-        return True
-    if old.width < new.width:
-        return True
-    if old.height < new.height:
-        return True
-    return False
 
 
 class NotifyWindowChangedReceiver():
@@ -32,84 +22,82 @@ class NotifyWindowChangedReceiver():
 
         old_cur = message.payload.data
         assert old_cur is not None
-        old_targets = old_cur.sub[Cursor.Targets]
 
         new_cur = await CursorHandler.get(cur_id)
-        if not validate(old_cur, new_cur):
+        if is_omittable(old_cur, new_cur):
             return
 
-        # 변경해야함
-        new_targets = await CursorHandler.get_targets(new_cur)
+        view_range = new_cur.view_range
+        tiles = await fetch_delta_tiles(view_range)
 
-        new_targets = Cursor.Targets(
-            _id=cur_id,
-            relations=[cur.id for cur in new_targets]
-        )
+        await multicast_tiles_state_event(target_conns=[new_cur], range=view_range, tiles_list=[tiles])
 
-        tl = new_cur.view_range.top_left
-        br = new_cur.view_range.bottom_right
-        tiles = await BoardHandler.fetch(tl, br)
+        new_targets = await get_new_targets(old_cur, new_cur)
 
-        tiles_state_event = make_tiles_state_event(new_cur, tiles)
-
-        await multicast(
-            target_conns=[cur_id],
-            event=tiles_state_event
-        )
-
-        filtering_targets = filter_new_targets(old_targets, new_targets)
-        if len(filtering_targets) == 0:
-            return
-
-        filtering_targets = [
-            await CursorHandler.get(_cur_id)
-            for _cur_id in filtering_targets
-        ]
-
-        event = await make_cursor_state_event(filtering_targets)
-        await multicast(
-            target_conns=[cur_id],
-            event=event
-        )
+        await multicast_cursor_state_event(target_conns=[new_cur], cursors=new_targets)
 
 
-async def make_cursor_state_event(targets: list[Cursor]):
-    event_elem_li = [
+def is_omittable(old: Cursor, new: Cursor):
+    if old.position != new.position:
+        return False
+    if old.width < new.width:
+        return False
+    if old.height < new.height:
+        return False
+    return True
+
+
+async def multicast_cursor_state_event(target_conns: list[Cursor], cursors: list[Cursor]):
+    elems = [
         ServerEvent.CursorsState.Elem(
-            id=cur.id,
-            position=cur.position,
-            pointer=cur.pointer,
-            color=cur.color,
-            revive_at=cur.revive_at,
-            score=(await ScoreHandler.get(cur.id)).value
+            id=c.id,
+            position=c.position,
+            pointer=c.pointer,
+            color=c.color,
+            revive_at=c.revive_at,
+            score=(await ScoreHandler.get(c.id)).value
         )
-        for cur in targets
+        for c in cursors
     ]
 
-    event = ServerEvent.CursorsState(
-        cursors=event_elem_li
-    )
+    event = ServerEvent.CursorsState(cursors=elems)
 
-    return event
+    await multicast(target_conns=[c.id for c in target_conns], event=event)
 
 
-def filter_new_targets(old_targets: Cursor.Targets, new_targets: Cursor.Targets):
+async def get_new_targets(old_cursor: Cursor[Cursor.Targets], new_cursor: Cursor) -> list[Cursor]:
+    old_targets = old_cursor.sub[Cursor.Targets]
+
+    # 현재 시야의 커서
+    current_targets = await CursorHandler.get_targets(new_cursor)
+
+    # 이전 상태의 커서들과 비교하여 새로운 커서를 찾기
+    new_targets = filter_new_targets(old_targets, current_targets)
+    return new_targets
+
+
+def filter_new_targets(old_targets: Cursor.Targets, new_targets: list[Cursor]) -> list[Cursor]:
     def func(id): return id not in old_targets
 
-    return Cursor.Targets(
-        _id=new_targets.id,
-        relations=[cur_id for cur_id in filter(func, new_targets)]
-    )
+    return list(filter(func, new_targets))
 
 
-def make_tiles_state_event(cursor: Cursor, tiles: Tiles):
-    event_elem = ServerEvent.TilesState.Elem(
-        range=cursor.view_range,
-        data=tiles.to_str()
-    )
+# TODO: 바뀐 부분만 fetch 할 수 있도록 인터페이스 변경.
+async def fetch_delta_tiles(range: PointRange) -> Tiles:
+    tiles = await BoardHandler.fetch(range.top_left, range.bottom_right)
 
-    event = ServerEvent.TilesState(
-        tiles=[event_elem]
-    )
+    return tiles
 
-    return event
+
+# TODO: tiles가 position 정보를 가져야 함.
+async def multicast_tiles_state_event(target_conns: list[Cursor], range: PointRange, tiles_list: list[Tiles]):
+    elems = [
+        ServerEvent.TilesState.Elem(
+            range=range,
+            data=tiles.to_str()
+        ) for tiles in tiles_list
+    ]
+
+    event = ServerEvent.TilesState(tiles=elems)
+
+    await multicast(target_conns=[c.id for c in target_conns], event=event)
